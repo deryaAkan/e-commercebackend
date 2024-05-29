7cefaf72664fda371343d0855b19667e4bee1527
Derya Akan <d.akan369@gmail.com>
2024-05-29 20:08:35 +0300

    dto records

--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.ecommerce.service;
 
+import com.example.ecommerce.entity.User;
+
 public interface UserService {
+   User findById(Long id);
 }
--- src/main/java/com/example/ecommerce/service/UserServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.service;
 
+import com.example.ecommerce.entity.User;
 import com.example.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,11 @@ public class UserServiceImpl implements UserService {
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        userRepository.findById(id);
+        return null;
     }
 }
